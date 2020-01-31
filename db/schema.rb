9fd9f57e0125aa8f819ed7f5c56fd1e13c658ef3
jiroubou <59195911+jiroubou@users.noreply.github.com>
2020-01-31 14:53:47 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2020_01_30_060504) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
