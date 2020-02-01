9c51b1522e88d157ae51fae40ed6f18d435676d7
jiroubou <59195911+jiroubou@users.noreply.github.com>
2020-02-01 16:21:12 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # devise_for :users
-  root 'test#sign_up_last'
+  root 'test#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
