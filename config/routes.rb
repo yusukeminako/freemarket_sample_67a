bd85fd4cf656d8224706030ca523ed1f3004c910
yusukeminako <yusuke19881016aspect@yahoo.co.jp>
2020-02-04 13:35:52 +0900

    second-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: "posts#index"
+  resources "test", only: [:index]
   resources "categorys", only: [:index]
+  resources "users", only: [:index, :new]
+  #get 'categorys/create', to: 'categorys#create'
+  #get 'categorys/new', to: 'categorys#new'
 end
+
