c09d5aeff52421ec7efd2d6ce8aeeed6b60c573d
jiroubou <59195911+jiroubou@users.noreply.github.com>
2020-02-04 11:22:55 +0900

    4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # devise_for :users
   root to: "posts#index"
-  #root 'test#index_new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources "test", only: [:index]
+  resources "test", only: [:index, :new]
   resources "categorys", only: [:index]
 end
