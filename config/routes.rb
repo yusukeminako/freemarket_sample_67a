ddd28c953128a37461f4df66953b6e442a3df30a
yusukeminako <yusuke19881016aspect@yahoo.co.jp>
2020-02-07 20:12:54 +0900

    nine-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
 root to: "posts#index"
-  get 'card/new'
-  get 'card/show'
-  root to: "posts#index"
+  resources "card", only: [:new, :show]
   resources "test", only: [:index, :new]
   resources "categorys", only: [:index]
   resources "users", only: [:index, :new]
   resources :card, only: [:index, :new, :show] do
     collection do
-      post 'show', to: 'card#show'
+      get 'show', to: 'card#show'
       post 'pay', to: 'card#pay'
-      post 'delete', to: 'card#delete'
+      post 'delete', to: 'card#destroy'
     end
   end
 end
