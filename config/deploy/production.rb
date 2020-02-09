92833b0e057d3b5748575a4b7c2910a06da3e86f
ararechan <59217931+ararechan@users.noreply.github.com>
2020-02-09 15:34:17 +0900

    dep_16

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,7 +9,8 @@ server '54.92.108.193', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
