8d8556dfb91f5c4ccb06b76885f03451f3619b71
Lucky0825 <ikedakaren@hotmail.co.jp>
2020-02-11 17:40:11 +0900

    saikidou

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :validatable, password_length: 7..128 
-  extend ActiveHash::Associations::ActiveRecordExtensions 
-  belongs_to_active_hash :year
-  belongs_to_active_hash :day
-  belongs_to_active_hash :month
-  
+  #  extend ActiveHash::Associations::ActiveRecordExtensions do
+  #  belongs_to_active_hash :year
+  #  belongs_to_active_hash :day
+  #  belongs_to_active_hash :month
+  #  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_02_10_051917) do
-  
+
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zip_code", null: false
     t.string "prefecture", null: false
@@ -35,12 +35,6 @@ ActiveRecord::Schema.define(version: 2020_02_10_051917) do
     t.string "name", null: false
     t.string "ancestry"
     t.index ["ancestry"], name: "index_categories_on_ancestry"
-  end
-
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "image", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
