d251205d0b249c2d0bac57379d3f2d28b270f1c9
yusukeminako <yusuke19881016aspect@yahoo.co.jp>
2020-02-16 19:13:05 +0900

    4commit

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -31,7 +31,7 @@ class CardController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     if card.blank?
     else
-      #  Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
       customer.delete
       card.delete
