4490ed9daa676a0873095e2dd448f9dd9ea59263
jiroubou <59195911+jiroubou@users.noreply.github.com>
2020-02-17 16:27:42 +0900

    a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    # @product = Product.find(3)
+    # @product = Product.find(3) 後ほど使う可能性あり
     @products = Product.all
     @images = Image.all
   end
