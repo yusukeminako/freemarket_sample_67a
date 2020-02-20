da553416dfabf063f3d2d9cf699bd9a1313edbfa
ararechan <59217931+ararechan@users.noreply.github.com>
2020-02-20 13:20:12 +0900

    migrateリセット

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_082400) do
+ActiveRecord::Schema.define(version: 2020_02_12_081119) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.integer "zip_code", null: false
     t.string "prefecture", null: false
     t.string "city", null: false
     t.string "address1", null: false
-    t.string "address2", null: false
+    t.string "address2"
     t.integer "landline"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -56,10 +58,10 @@ ActiveRecord::Schema.define(version: 2020_02_19_082400) do
     t.bigint "buyer_id"
     t.integer "size"
     t.bigint "prefecture_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "shipping_burden"
     t.string "shipping_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["buyer_id"], name: "index_products_on_buyer_id"
     t.index ["category_id"], name: "index_products_on_category_id"
